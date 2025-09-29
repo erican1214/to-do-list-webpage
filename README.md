7b8ada39cf8ac86b6ba3acd3f8fff6ec1c87256c
erican1214 <erican1214@outlook.com>
2025-09-28 22:47:40 -0400

    node folder

---
+++ README.md
@@ -0,0 +1,56 @@
+# To-Do List Webpage
+## About
+This webpage is for you to type any tasks you want to do and save them temporarily. Reloading the webpage will erase your tasks. The task functions are somewhat limited because the webpage is more focused on creating an aesthetically pleasing interface. The colors, pictures, and audio are themed after Love and Deepspace. (As of now, there are no voice files, so only the music button workds.)
+
+## Important Information
+### Applications Needed
+- Node.js
+- VSCode (or any IDE)
+- Google Chrome (or any browser)
+
+### Packages Needed (Should already be in repository)
+- Express.js (this is the node_modules folder, package.json, and package-lock.json)
+
+- If any of the packages are said to be missing, navigate to the project directory, and run the following command prompts (for Windows, use npm.cmd instead of npm)
+    - npm init -y
+    - npm install express
+
+- More command prompts if necessary
+    - npm install ejs
+
+### How to run webpage
+- Navigate the project directory
+- Type "node server.js" if you are only interacting
+- Type "npm run dev" if you want to continuously edit this webpage (for Windows, use npm.cmd instead of npm)
+- Go on your browser and type "localhost:3000" in the search bar
+
+### Compatability/Accessibility
+- I have only tested this webpage on a Google Chrome Browser. Let me know if it is not compatible with other browsers.
+- Although this is responsive on mobile devices, a couple of features are hidden when used on mobile devices in portrait mode. The width of webpage must be at least 600px to access all features
+
+### Image Attributions
+- [audio_icon.png](https://www.freepik.com/free-vector/bright-megaphone-design_138418344.htm)
+- [checkmark.png](https://www.flaticon.com/free-icons/checkbox)
+- [music_icon.png](https://www.freepik.com/free-vector/bright-music-note_145290248.htm)
+- [star_button.png](https://www.freepik.com/free-vector/star-user-interfase-button_137585895.htm)
+
+- All other images, voicelines, and music are from Love and Deepspace by Paper Games.
+
+## File Overview
+&larr; README.md
+- This file currently, shows details about how this webpage works
+
+&larr; index.ejs
+- HTML file that shows content of the webpage
+
+&larr; style.css
+- CSS file that customizes that content of the webpage
+
+&larr; script.js
+- Javascript file that adds interaction to the webpage and listens for events
+
+&larr; server.js
+- Javascript file that connects the code to server
+
+&larr; package.json, package-lock.json
+- Shows the necessary libraries/packages/dependencies for Node.js to run Express.js
---
+++ public/script.js
@@ -0,0 +1,240 @@
+const taskList = document.getElementsByClassName("task");
+const completedTaskList = document.getElementsByClassName("completedTask");
+
+const section = document.getElementsByClassName("section");
+const removeButton = document.getElementsByClassName("removeButton");
+const addTaskButton = document.getElementById("addTaskButton");
+
+let image = document.getElementById("image");
+let error = document.getElementById("error");
+
+const xavierImages = {
+    "default": "assets/images/xavier_default.webp",
+    "happy": "assets/images/xavier_happy.webp",
+    "sad": "assets/images/xavier_sad.webp",
+    "sleepy": "assets/images/xavier_sleepy.webp"
+};
+
+let musicIcon = document.getElementById("musicIcon");
+let audioIcon = document.getElementById("audioIcon");
+const musicButton = document.getElementById("musicButton");
+const audioButton = document.getElementById("audioButton");
+const backgroundMusic = document.getElementById("backgroundMusic");
+
+const musicImages = {
+    "muted": "assets/images/muted_music_icon.png",
+    "unmuted": "assets/images/music_icon.png"
+};
+
+const audioImages = {
+    "muted": "assets/images/muted_audio_icon.png",
+    "unmuted": "assets/images/audio_icon.png"
+};
+
+const clearCurrentTasks = document.getElementById("clearCurrentTasks");
+const clearCompletedTasks = document.getElementById("clearCompletedTasks");
+
+/*
+    Adds task to list.
+    Since task is only added if there is no error,
+    the error message is cleared if there was one
+    previously
+*/
+function addTask(taskMsg) {
+    error.innerHTML = "";
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].innerHTML === "") {
+            taskList[i].innerHTML = taskMsg;
+            section[i % (section.length/2)].style.display = "flex";
+            break;
+        }
+    }
+}
+
+/* 
+    Makes sure there are no dupicate tasks,
+    even if they are in different casing
+*/
+function compareTask(taskMsg) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].innerHTML.toLowerCase() === taskMsg.toLowerCase()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/* 
+    Removes the task the user clicked.
+    Calls function reOrganize() to reogranize current task list.
+    Calls function moveToComplete() to move finished/removed task
+    to the completed task list.
+    Calls changeImage("happy") to change the character image to
+    "happy" to indicate the user did a good job
+*/
+function removeTask(taskNum) {
+    const finishedTask = taskList[taskNum].innerHTML;
+    taskList[taskNum].innerHTML = "";
+    section[taskNum % (section.length / 2)].style.display = "none";
+    reOrganize(taskNum);
+    moveToComplete(finishedTask, 0);
+    changeImage("happy");
+}
+
+/*
+    Shifts the bottom tasks up if a task in the middle of the list
+    was removed
+*/
+function reOrganize(taskNum) {
+    for (let i = taskNum; i < taskList.length-1; i++) {
+        if (taskList[i].innerHTML === "" && taskList[i+1].innerHTML === "") {
+            break;
+        }
+        else {
+            taskList[i].innerHTML = taskList[i+1].innerHTML;
+            section[i % (section.length/2)].style.display = "flex";
+            taskList[i+1].innerHTML = "";
+            section[(i+1) % (section.length/2)].style.display = "none";
+        }
+    }
+}
+
+/*
+    Moves the finished/removed task to the top of completed section.
+    Shifts the older completed tasks down recursively
+*/
+function moveToComplete(finishedTask, num) {
+    let currTask = completedTaskList[num].innerHTML;
+    if (num == completedTaskList.length) {
+        return;
+    }
+    else if (currTask === "") {
+        completedTaskList[num].innerHTML = finishedTask;
+        section[num + (section.length/2)].style.display = "flex";
+        return;
+    }
+    else {
+        completedTaskList[num].innerHTML = finishedTask;
+        section[num + (section.length/2)].style.display = "flex";
+        moveToComplete(currTask, num+1);
+    }
+}
+
+/*
+    Changes expression of the character image depending on parameter
+    "expression," waits 3 seconds, then changes back to default image
+*/
+function changeImage(expression) {
+    image.src = xavierImages[expression];
+    setTimeout(event => {
+        image.src = xavierImages["default"];
+    }, 3000);
+}
+
+/*
+    Checks for bonus words and changes character image based on bonus word
+*/
+function checkBonusWords(taskMsg) {
+    lowercaseMsg = taskMsg.toLowerCase();
+    if (lowercaseMsg.includes("sleep")) {
+        changeImage("sleepy");
+    }
+}
+
+/*
+    Plays music if music is muted, and mutes music if music is playing
+    Updates music icon and tooltip based on state of music
+*/
+function playOrMuteMusic() {
+    if (backgroundMusic.muted) {
+        musicIcon.src = musicImages["unmuted"];
+        backgroundMusic.muted = false;
+        backgroundMusic.play();
+        backgroundMusic.volume = 0.2;
+        musicButton.dataset.title = "Mute music?";
+    }
+    else {
+        musicIcon.src = musicImages["muted"];
+        backgroundMusic.muted = true;
+        backgroundMusic.pause();
+        musicButton.dataset.title = "Play music?";
+    }
+}
+
+/*
+    Clears either current or completed task list based on parameter
+    "list"
+    Hides each task section and clears innerHTML of each task
+*/
+function clearTasks(list) {
+    if (list === "current") {
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList.innerHTML === "") {
+                break;
+            }
+            taskList[i].innerHTML = "";
+            section[i].style.display = "none";
+        }
+    }
+    else {
+        for (let i = 0; i < completedTaskList.length; i++) {
+            if (completedTaskList.innerHTML === "") {
+                break;
+            }
+            completedTaskList[i].innerHTML = "";
+            section[i + (section.length/2)].style.display = "none";
+        }
+    }
+}
+
+/*
+    Below are the added button event listeners and their functions
+*/
+if (addTaskButton) {
+    addTaskButton.addEventListener("click", event => {
+        const taskMsg = document.getElementById("taskMsg");
+        const newTaskMsg = taskMsg.value.trim();
+        if (newTaskMsg === "") {
+            error.innerHTML = "Can't submit an empty task!";
+            changeImage("sad");
+        }
+        else if (taskList[taskList.length-1].innerHTML != "") {
+            error.innerHTML = "Can't have more than " + taskList.length + " tasks!";
+            changeImage("sad");
+        }
+        else if (compareTask(newTaskMsg)) {
+            error.innerHTML = "You already have this task in the list!";
+            changeImage("sad");
+        }
+        else {
+            addTask(newTaskMsg);
+            checkBonusWords(newTaskMsg);
+        }
+    })
+}
+
+if (removeButton) {
+    for (let i = 0; i < removeButton.length; i++) {
+        removeButton[i].addEventListener("click", event => {
+            removeTask(i)
+        });
+    }
+}
+
+if (musicButton) {
+    musicButton.addEventListener("click", event => {
+        playOrMuteMusic();
+    });
+}
+
+if (clearCurrentTasks) {
+    clearCurrentTasks.addEventListener("click", event => {
+        clearTasks("current");
+    })
+}
+
+if (clearCompletedTasks) {
+    clearCompletedTasks.addEventListener("click", event => {
+        clearTasks("completed");
+    })
+}
